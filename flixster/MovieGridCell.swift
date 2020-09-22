98ab48846d5f7b6386f53796ed0a6df78fe80ea2
Gary Feng <garyfeng961@gmail.com>
2020-09-22 00:28:02 -0400

    Update Flixster

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Gary Feng on 9/20/20.
+//  Copyright © 2020 Gary Feng. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
